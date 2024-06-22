59946b9570502e188a7f31c3cfc9288ecd2f091f
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-06-23 00:31:06 +0530

    Update chat.model.js

--- Models/chat.model.js
+++ Models/chat.model.js
@@ -2,20 +2,32 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const messageSchema = new Schema({
-    sender_id: { type: String, required: true },
-    receiver_id: { type: String, required: true },
-    content: { type: String, default: null },
-    content_type: { type: String, enum: ["text", "file", "image"], required: true },
-    content_link: { type: String, default: null },
-    messageSid : {type : String , default : null},
-    accountSid : {type : String , default : null},
-    timestamp: { type: Date, default: Date.now },
-    is_read: { type: Boolean, default: false }
+  sender_id: { type: String, required: true },
+  receiver_id: { type: String, required: true },
+  content: { type: String, default: null },
+  content_type: {
+    type: String,
+    enum: [
+      "text",
+      "file",
+      "image/jpeg",
+      "image/png",
+      "video/mp4",
+      "audio/mpeg",
+      "application/pdf",
+    ],
+    required: true,
+  },
+  content_link: { type: String, default: null },
+  messageSid: { type: String, default: null },
+  accountSid: { type: String, default: null },
+  timestamp: { type: Date, default: Date.now },
+  is_read: { type: Boolean, default: false },
 });
 
 const conversationSchema = new Schema({
-    participant : {type: String , required : true},
-    messages: [messageSchema],
+  participant: { type: String, required: true },
+  messages: [messageSchema],
 });
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
